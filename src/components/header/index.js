a36183c7be22789d501ce0d712b633221f8c38cf
Alysson Wanderley <105825366+alyssonwa@users.noreply.github.com>
2026-04-09 19:30:28 -0300

    estrutura inicial e inicio do componente header

---
+++ src/components/header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css';
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>Alysson Alves</h1>
+        <nav>
+          <a href="#">Início</a>
+          <a href="#">Projetos</a>
+          <a href="#">Sobre Mim</a>
+          <a href="#">Contato</a>
+          <a href="#">Currículo</a>
+        </nav>
+      </header>
+    </>
+  );
+};
+export default Header;
