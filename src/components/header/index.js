2ada718fb7b074c3a65f39268e7e4eddf65127f0
Alysson Wanderley <105825366+alyssonwa@users.noreply.github.com>
2026-04-10 16:34:17 -0300

    alteração da estrutura do componente header para mobile first, add biblioteca do material icons e flexbox no header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './index.css';
+import './media-query.css';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
   return (
     <>
       <header>
         <h1>Alysson Alves</h1>
+        <MenuIcon id="botao-menu" onclick="clickMenu()" />
         <nav>
           <a href="#">Início</a>
           <a href="#">Projetos</a>
