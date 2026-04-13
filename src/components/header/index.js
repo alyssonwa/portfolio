c4dafaa818f8ff018ec116b5f86e9ca4cc6ed415
Alysson Wanderley <105825366+alyssonwa@users.noreply.github.com>
2026-04-13 16:16:37 -0300

    organizando header e começando a implementar botao de menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import './media-query.css';
 import MenuIcon from '@mui/icons-material/Menu';
+import LogoEstrela from '../logo';
 
 const Header = () => {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const alternarMenu = () => {
+    setMenuAberto(!menuAberto);
+  };
+
   return (
     <>
       <header>
-        <h1>Alysson Alves</h1>
-        <MenuIcon id="botao-menu" onclick="clickMenu()" />
-        <nav>
+        <h1>
+          <LogoEstrela />
+          Alysson Alves
+        </h1>
+        <button
+          className="botao-menu"
+          onClick={alternarMenu}
+          aria-label="Abrir menu" // Boa prática para acessibilidade!
+        >
+          <MenuIcon />
+        </button>
+        <nav className={`nav-menu ${menuAberto ? 'ativo' : ''}`}>
           <a href="#">Início</a>
           <a href="#">Projetos</a>
           <a href="#">Sobre Mim</a>
