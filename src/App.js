919e2b8359f494ea01b06adee76fd8735286e223
Alysson Wanderley <105825366+alyssonwa@users.noreply.github.com>
2026-04-20 19:07:40 -0300

    adicionando efeito de luz que acompanha cursor do mouse na tela principal (spotlight)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.css';
 import Header from './components/header';
 import Resumo from './components/resumo';
 
 function App() {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    if (window.innerWidth >= 570) {
+      setMousePos({ x: e.clientX, y: e.clientY });
+    }
+  };
   return (
-    <>
+    <div
+      onMouseMove={handleMouseMove}
+      className={styles.container}
+      style={{
+        '--mouse-x': `${mousePos.x}px`,
+        '--mouse-y': `${mousePos.y}px`,
+      }}
+    >
+      <div className={styles.spotlight}></div>
+
       <Header />
-      {/* <Resumo /> */}
-    </>
+      <Resumo />
+    </div>
   );
 }
 export default App;
