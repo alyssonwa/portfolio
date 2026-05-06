9ae5279d42deeea97a213fc49ca68cba58f1e72d
Alysson Wanderley <105825366+alyssonwa@users.noreply.github.com>
2026-05-06 16:40:46 -0300

    limpando codigo mobile e desktop da parte de projetos, estabelecendo padroes para as divs das seções e fazendo configuração da capa do tavern talk

--- src/components/projetos/index.js
+++ src/components/projetos/index.js
@@ -3,12 +3,13 @@ import styles from './Projetos.module.css';
 import CelMockup from '../mockups/celular';
 import PcMockup from '../mockups/pc';
 import PrintSonhario from './CapasProjetos/sonhario/sonhario.png';
+import PrintTavernTalk from './CapasProjetos/tavernTalk/tavern_talk.png';
 
 const Projetos = () => {
   return (
     <main className={styles.projetosContainer}>
       <h1>Meus Projetos</h1>
-      <section>
+      <section className={styles.sonhario}>
         <div className={styles.projetoImagem}>
           <div className={styles.celularMockup}>
             <CelMockup className={styles.mockupSvg} />
@@ -25,20 +26,37 @@ const Projetos = () => {
           <button>Saiba mais...</button>
         </div>
       </section>
-      <section>
+      <section className={styles.tavernTalk}>
         <div className={styles.projetoImagem}>
           <div className={styles.celularMockup}>
             <CelMockup className={styles.mockupSvg} />
-            {/* <img
-              src={PrintSonhario}
-              alt="Tela inicial Sonhario"
+            <img
+              src={PrintTavernTalk}
+              alt="Tela de ficha Tavern Talk"
               className={styles.print}
-            /> */}
+            />
           </div>
         </div>
         <div className={styles.projetoDescricao}>
           <h1>Tavern Talk</h1>
           <p>Aplicativo web gerador de fichas de RPG</p>
+          <button>Saiba mais...</button>
+        </div>
+      </section>
+      <section className={styles.gap}>
+        <div className={styles.projetoImagem}>
+          <div className={styles.pcMockup}>
+            <PcMockup className={styles.mockupSvg} />
+            {/* <img
+              src={PrintTavernTalk}
+              alt="Tela de ficha Tavern Talk"
+              className={styles.print}
+            /> */}
+          </div>
+        </div>
+        <div className={styles.projetoDescricao}>
+          <h1>GAP</h1>
+          <p>Site de gerenciamento de ações</p>
           <button>Saiba mais...</button>
         </div>
       </section>
